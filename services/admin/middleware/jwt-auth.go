834fc595496339302874e281a4bd47b4fe116827
cjendeath <57334672+cjendeath@users.noreply.github.com>
2022-07-10 21:54:19 +0700

    master

--- services/admin/middleware/jwt-auth.go
+++ services/admin/middleware/jwt-auth.go
@@ -1,1 +1,34 @@
 package middleware
+
+import (
+	"chilindo/services/admin/helper"
+	"chilindo/services/admin/service"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+//AuthorizeJWT validates thr token user given, return 401 if not valid
+func AuthorizeJWT(jwtService service.JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		authHeader = strings.TrimPrefix(authHeader, "Bearer ")
+		if authHeader == "" {
+			response := helper.BuildErrorResponse("Failed to process request", "No token found", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		token, err := jwtService.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[user_id]:", claims["user_id"])
+			log.Println("Claim[issuer]:", claims["issuer"])
+		} else {
+			log.Println(err)
+			response := helper.BuildErrorResponse("Token is not valid", err.Error(), nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+	}
+}
--- services/admin/repository/admin-repository.go
+++ services/admin/repository/admin-repository.go
@@ -1,4 +1,43 @@
 package repository
 
+import (
+	"chilindo/services/admin/entity"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"log"
+)
+
 type AdminRepository interface {
+	UpdateAdmin(admin entity.Administrator) entity.Administrator
+}
+type adminConnection struct {
+	connection *gorm.DB
+}
+
+func NewAdminRepository(db *gorm.DB) AdminRepository {
+	return &adminConnection{
+		connection: db,
+	}
+}
+func (db adminConnection) UpdateAdmin(admin entity.Administrator) entity.Administrator {
+	//TODO implement me
+	if admin.Password != "" {
+		admin.Password = hashAndSalt([]byte(admin.Password))
+	} else {
+		var tempUser entity.Administrator
+		db.connection.Find(&tempUser, admin.Id)
+		admin.Password = tempUser.Password
+	}
+
+	db.connection.Save(&admin)
+	return admin
+}
+
+func hashAndSalt(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+		panic("Failed to hash a password")
+	}
+	return string(hash)
 }
