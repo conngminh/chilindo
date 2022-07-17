473160668e2af5038dd039c9a238a3ebe6d3582a
Lê Đoàn <52238710+ledoanmk43@users.noreply.github.com>
2022-07-17 21:50:16 +0700

    fix issues

--- src/product-service/route/product-image.go
+++ src/product-service/route/product-image.go
@@ -23,10 +23,10 @@ func (i ImageRoute) GetRouter() {
 	imageRoutes := i.Router.Group("api/image")
 	imageRoutes.Use(middleware.Logger())
 	{
-		imageRoutes.POST("/:productId/product", i.ProductImageController.CreateImage)
-		imageRoutes.GET("/:productId/product", i.ProductImageController.GetImage)
-		//imageRoutes.DELETE("/:imageId", i.ProductImageController.DeleteImage)
-		//imageRoutes.GET("/:imageId", i.ProductImageController.GetImageByID)
-		//imageRoutes.PUT("/:imageId", i.ProductImageController.UpdateImage)
+		imageRoutes.POST("/id=:productId", i.ProductImageController.CreateImage)
+		imageRoutes.GET("/:productId", i.ProductImageController.GetImage)
+		imageRoutes.DELETE("/:imageId", i.ProductImageController.DeleteImage)
+		imageRoutes.GET("/image/:imageId", i.ProductImageController.GetImageByID)
+		imageRoutes.PUT("/:imageId", i.ProductImageController.UpdateImage)
 	}
 }
